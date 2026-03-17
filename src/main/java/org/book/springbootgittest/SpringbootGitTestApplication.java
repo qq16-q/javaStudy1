4f0d87b243172fadd891c48eaccfb2b90007c918
qq16-q <3077261518@qq.com>
2026-03-17 21:20:51 +0800

    Initial commit

---
+++ src/main/java/org/book/springbootgittest/SpringbootGitTestApplication.java
@@ -0,0 +1,13 @@
+package org.book.springbootgittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGitTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/book/springbootgittest/SpringbootGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.book.springbootgittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
